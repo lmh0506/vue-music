b9c6bc64ffab3a4011e3678e91e511253f35cc98
lmh <495453865@qq.com>
2017-08-28 18:15:06 +0800

    用户中心界面完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Singer from '@/components/singer/singer'
 import SingerDetail from '@/components/singer-detail/singer-detail'
 import Disc from '@/components/disc/disc'
 import TopList from '@/components/top-list/top-list'
+import UserCenter from '@/components/user-center/user-center'
 
 Vue.use(Router)
 
@@ -52,6 +53,11 @@ export default new Router({
         path: ':id',
         component: SingerDetail
       }]
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserCenter
     }
   ]
 })
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 import * as types from './mutation-types'
 import {playMode} from 'assets/js/config'
 import {shuffle} from 'assets/js/util'
-import {saveSearch, deleteSearch, clearSearch, savePlay} from 'assets/js/cache'
+import {saveSearch, deleteSearch, clearSearch, savePlay, saveFavorite, deleteFavorite} from 'assets/js/cache'
 
 function findIndex(list, song) {
   return list.findIndex(item => {
@@ -119,4 +119,12 @@ export const clearSearchHistory = function({commit}) {
 
 export const savePlayHistory = function({commit}, song) {
   commit(types.SET_PLAY_HISTORY, savePlay(song))
+}
+
+export const saveFavoriteList = function({commit}, song) {
+  commit(types.SET_FAVORITE_LIST, saveFavorite(song))
+}
+
+export const deleteFavoriteList = function({commit}, song) {
+  commit(types.SET_FAVORITE_LIST, deleteFavorite(song))
 }
